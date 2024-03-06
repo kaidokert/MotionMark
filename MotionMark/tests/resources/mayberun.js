a5e1200f72af971d7c565a43ee76cff3c5900cbf
Kaido Kert <kaidokert@gmail.com>
2024-03-06 08:20:15 -0800

    Customize via url params

--- MotionMark/tests/resources/mayberun.js
+++ MotionMark/tests/resources/mayberun.js
@@ -1,29 +1,18 @@
 
-window.setTimeout(() => {
-    console.log("maybe run ?");
-    var benchmark = new window.benchmarkClass({
-
-        "complexity": "100",
+function maybeRun() {
+    let qs = Utilities.convertQueryStringToObject(location.search);
+    let opts = window.benchmarkController.benchmarkDefaultParameters;
+    let conf = {
+        "complexity": "50",
         "configuration": "medium",
-        "controller": "fixed",
-        "display": "minimal",
         "fill": "gradient",
-        "first-frame-minimum-length": 0,
-        "frame-rate": 120,
-        "kalman-measurement-error": 4,
-        "kalman-process-error": 1,
         "particleHeight": "50",
         "particleWidth": "50",
         "shape": "circle",
-        "system-frame-rate": 120,
-        "test-interval": 10,
-        "tiles": "big",
-        "time-measurement": "performance",
-        "warmup-frame-count": 30,
-        "warmup-length": 2000,
-
-    });
-    console.log(benchmark)
+    };
+    Object.assign(opts, conf)
+    Object.assign(opts, qs)
+    var benchmark = new window.benchmarkClass(opts);
     benchmark.run().then((testData) => {
         console.log("testData", testData);
         var db = new ResultsDashboard(1,
@@ -34,6 +23,7 @@ window.setTimeout(() => {
         );
         db.calculateScore(testData);
         console.log("score", JSON.stringify(testData.result));
-        // motionmark.js: calculateScore(testData)
     });
-}, 10)
+}
+
+window.addEventListener("load", function() { maybeRun(); });
